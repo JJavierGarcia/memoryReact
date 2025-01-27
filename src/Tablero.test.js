a1abb875cea78689531f660a1026f6201bbd4fea
José Javier García Sánchez <josejavier.garcia@informatica.cifpaviles.pa>
2025-01-27 17:26:48 +0100

    Prueba

--- src/Tablero.test.js
+++ src/Tablero.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Tablero from './Tablero';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Tablero />);
   const linkElement = screen.getByText(/memory/i);
   expect(linkElement).toBeInTheDocument();
 });
